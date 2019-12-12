7330716ab40d3632e418b9f1d0a8b185eb60f34c
maarcospaaulo <maarcospaaulo@live.com>
2019-12-12 10:36:56 -0300

    Aplicando SASS

--- js/form.js
+++ js/form.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $(".formulario__campo").click(function() { // click -> borda do input azul
-        $( this ).css("outline-color", "#00A4EF") // OBS: tambem fiz isso apenas com CSS em (css/style) Line 602
+        $( this ).css("outline-color", "#00A4EF") // OBS: tambem fiz isso apenas com CSS em css/style comentado
     });
 });
